99fa5478824a6205fbc3b2698a198710632597a8
Ubuntu <ubuntu@ip-172-31-4-214.us-east-2.compute.internal>
2019-09-26 06:06:37 +0000

    run_face_emb

--- data/run_face_emb.sh
+++ data/run_face_emb.sh
@@ -1,1 +1,2 @@
-python -W ignore extract_fabnet.py --bsfldr 'data/vox2_test_mp4' --njobs 2
+python -W ignore extract_fabnet.py --bsfldr 'vox2_test_mp4' --njobs 10
+python -W ignore extract_fabnet.py --bsfldr 'vox2_mp4' --njobs 10
